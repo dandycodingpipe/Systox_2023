b011ee7178635c23cd51207ca419bfd57b66e6e9
dandycodingpipe <christian.a.hernandezf@hotmail.com>
2023-02-13 15:18:27 +0100

    Recreates the script as a function. This will make my NLP phase of the data pre-processing much simpler.

--- PubMed_Extractor.R
+++ PubMed_Extractor.R
@@ -1,5 +1,4 @@
-#Goal for today: create an effective data frame (PMID, Year, Abstract)
-#of specific PubMed Entrez queries
+#This script creates a function that returns a data frame of extractable information from a publication
 
 library(easyPubMed)
 library(dplyr)
@@ -8,29 +7,29 @@ library(kableExtra)
 #important packages for the organization
 
 
+#my_query <- 'Bladder[TIAB] AND Northwestern[AD] AND Chicago[AD] AND "2013"[PDAT]:"2023"[PDAT]'
+#my_query <- get_pubmed_ids(my_query)
 
-#1) R query (you must learn more Entrez/Query synatx to optimize your search)
-
-my_query <- 'Bladder[TIAB] AND Northwestern[AD] AND Chicago[AD] AND "2013"[PDAT]:"2023"[PDAT]'
-my_query <- get_pubmed_ids(my_query)
-
-#Fetch Data
-
-my_abstracts_xml <- fetch_pubmed_data(my_query)
-
-#Consolidating Abstracts into a list
-all_xml <- articles_to_list(my_abstracts_xml)
-
-#end pubmed query data preparation
-
-t.start <- Sys.time()
-
-final_df <- do.call(rbind, lapply(all_xml, article_to_df,max_chars = -1, getAuthors = FALSE))
-
-t.end <- Sys.time()
-print(t.end - t.start)
-
-#this function outputs in HTML a chouette table
-
-final_df[,c("pmid", "year", "abstract")] %>% kable() %>% kable_styling(bootstrap_options = 'striped')
-
+Abstract_Extractor <- function(Query_String) {
+  
+  #1) PMID Retrieval of Query
+  query <- Query_String
+  my_query <- get_pubmed_ids(my_query)
+  
+  #2) XML Retrieval
+  my_abstracts_xml <- fetch_pubmed_data(my_query)
+  all_xml <- articles_to_list(my_abstracts_xml)
+  
+  #3) XML Conversion to data frame with computation time
+  
+  t.start <- Sys.time()
+  
+  final_df <- do.call(rbind, lapply(all_xml, article_to_df,max_chars = -1, getAuthors = FALSE))
+  
+  t.end <- Sys.time()
+  print(t.end - t.start)
+  
+  #HTML Formatting (reference)
+  #final_df[,c("pmid", "year", "abstract")] %>% kable() %>% kable_styling(bootstrap_options = 'striped')
+  return(final_df[,c('pmid','year', 'abstract')])
+}
