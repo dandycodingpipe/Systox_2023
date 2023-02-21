487347bea6d7a00607e013233297caf31e7dd727
Chris <christian.a.hernandezf@hotmail.com>
2023-02-21 09:47:11 +0100

    Commiting functioning parser

--- SpacyR_TextParser.R
+++ SpacyR_TextParser.R
@@ -8,20 +8,17 @@ my_query <- 'Bladder[TIAB] AND Northwestern[AD] AND Chicago[AD] AND "2013"[PDAT]
 raw_pubmed_df <- Abstract_Extractor(my_query)
 raw_pubmed_df$abstract
 
-install.packages('spacyr')
 library(spacyr)
-spacy_install(lang_models = 'en_core_web_sm')
 
-#spacyR comes from python and its incredibly useful for condensing all of the NLP info
-#you need into one data frame!!!!!
-
-spacy_initialize(model = 'en_core_web_sm')
-spacy_download_langmodel(model = 'en')
-
+#C:\Users\Chris\.virtualenvs\Python-FsgYzr6a>C:\Users\Chris\.virtualenvs\Python-FsgYzr6a\Scripts\activate.bat
+spacy_download_langmodel(model = "en_core_web_trf", virtualenv_root = "C:\\Users\\Chris\\.virtualenvs\\Python-FsgYzr6a")
+spacy_initialize(model = "en_core_web_sm", virtualenv = "C:\\Users\\Chris\\.virtualenvs\\Python-FsgYzr6a")
+papers_3 <- spacy_parse(raw_pubmed_df$abstract[9:11])
+#spacy_install(conda = 'auto' , version = 'latest', lang_models = 'en-core_web_sm', python_version = '3.6')
 #DISCLAIMER: Before you can initialize Spacy in R, you must have spacy installed for Python in your computer
 #I did it through conda
 
-txt <- raw_pubmed_df$abstract[1]
+#update
 
 t.start <- Sys.time()
 parsedtxt <- spacy_parse(txt)
