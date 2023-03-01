03c51f7839ba8f5f43a178c11203de3118db931c
Chris <christian.a.hernandezf@hotmail.com>
2023-03-01 16:26:30 +0100

    3/1 Commit: I made Text_Parsing into its own function to streamline the workflow. Also am working on a reduced search function as AOP-helpFinder cited it improving results.

--- SpacyR_TextParser.R
+++ SpacyR_TextParser.R
@@ -4,63 +4,58 @@
 
 library(spacyr)
 
-source("PubMed_Extractor.R")
-
-my_query <- 'complement system AND (pathology OR pathologies)'
-raw_pubmed_df <- Abstract_Extractor(my_query, 1000)
-
-#text parser creates a parsed data frame of the extracted abstracts. a path to a python virtual environment
-#must be called as well as the spacy language model
-Text_Parser <- function(data, venv_path, lang_model){
+Abstract_Trimmer <- function(raw_data, fraction){
   
+
+  NAs <- which(is.na(raw_data$abstract))
+  raw_data <- raw_data[-NAs,]
+  print(which(is.na(raw_data$abstract)))
+  for(i in 1:length(raw_data$abstract)){
+      
+      total <- nchar(raw_data$abstract)
+      new_length <- total - round((fraction*total), digits = 0)
+      new_length <- as.integer(new_length)
+      print(new_length)
+  
+      raw_data$abstract[i] <- str_trunc(raw_data$abstract[i], new_length, 'left')
+  }
+  
+  return(raw_data)
+}
+
+Text_Parser <- function(data, venv_path, lang_model, reduced_search){
+  
+  #abstract reduced search
+  Abstract_Trimmer(data, reduced_search)
+  #spacy environment creation and text parsing
   spacy_initialize(model = lang_model, virtualenv = venv_path )
-  parsed_text <- spacy_parse(raw_pubmed_df$abstract)
+  print("spaCy initialized. Parsing the abstracts...")
+  parsed_text <- spacy_parse(data$abstract)
+  print(paste(length(parsed_text$lemma),"lemma found in the corpus. Filtering Parts-of-Speech..."))
+  
+  
+  #POS filtering and data cleaning
+  NOUN_POS <- which(parsed_text$pos == "NOUN")
+  VERB_POS <- which(parsed_text$pos == "VERB")
+  ADJ_POS <- which(parsed_text$pos == "ADJ")
+  
+  MASTER_POS <- c(NOUN_POS,VERB_POS,ADJ_POS)
+  parsed_text <- parsed_text[MASTER_POS,]
+  print(paste("Filtered ",length(parsed_text$lemma),"potentially relevant lemma. Generating lemma frequency graph..."))
+  
+  #item frequency pre-ARM
+  word_frequency <- parsed_text %>% count(lemma) %>%arrange(desc(n))
+  top20words <- head(word_frequency,20)
+  ggplot(top20words, aes(x = lemma, y = n, fill = lemma)) + geom_col() + ggtitle("Top 20 Words in Query")
+  #for some reason this is not working
+  spacy_finalize()
+  print('Text parsing complete!')
+  
   return(parsed_text)
 }
 
 
-#C:\Users\Chris\.virtualenvs\Python-FsgYzr6a>C:\Users\Chris\.virtualenvs\Python-FsgYzr6a\Scripts\activate.bat
-spacy_initialize(model = "en_core_web_sm", virtualenv = "C:\\Users\\Chris\\.virtualenvs\\Python-FsgYzr6a")
-papers_3 <- spacy_parse(raw_pubmed_df$abstract, entity = TRUE)
-#spacy_install(conda = 'auto' , version = 'latest', lang_models = 'en-core_web_sm', python_version = '3.6')
-#DISCLAIMER: Before you can initialize Spacy in R, you must have spacy installed for Python in your computer
-#I did it through conda
 
-
-#this is where a word frequenct counter is necessary
-#removing some keywords that bloat my output!
-STUDY <- which(papers_3$lemma == "study")
-EFFECT <- which(papers_3$lemma == "effect")
-CHEMICAL <- which(papers_3$lemma == "chemical")
-STOP <- which(papers_3$lemma == "-")
-STOP2 <- which(papers_3$lemma == "%")
-REMOVE <- c(STUDY,EFFECT, STOP, STOP2)
-papers_3 <- papers_3[-REMOVE,]
-which(papers_3$lemma == "study") #should be 0
-
-#i want to focus on nouns, verbs, and adjectives!
-concat <- nounphrase_consolidate(papers_3)
-
-NOUN_POS <- which(concat$pos == "nounphrase")
-VERB_POS <- which(concat$pos == "VERB")
-ADJ_POS <- which(concat$pos == "ADJ")
-
-master_pos <- c(NOUN_POS,VERB_POS,ADJ_POS)
-concat <- concat[master_pos,]
-
-which(concat$lemma == "study")
-
-papers_3 <- papers_3[NOUN_POS,]
-frequency_dataframe1 = papers_3 %>% count(lemma) %>% arrange(desc(n))
-nounphrase_freq = concat %>% count(lemma) %>% arrange(desc(n))
-
-short_dataframe1 = head(frequency_dataframe1, 20)
-ggplot(short_dataframe1, aes(x = lemma, y = n, fill = lemma)) + geom_col() 
-
-papers_3
-write.csv(concat, file = "data.csv")
-
-spacy_finalize()
 
 
 
