7c13d2b7d20ef28dd0bb50287baa4467b7e4d4d5
Chris <christian.a.hernandezf@hotmail.com>
2023-03-02 17:22:06 +0100

    3/2 final commit. The program is able to run on minimal code now... the next big challenge is really maximizing the discovery of relevant associations.

--- main.R
+++ main.R
@@ -2,34 +2,39 @@
 library(tidyverse)
 library(tidytext)
 library(tm)
+library(arules)
+library(arulesViz)
 
-source("Abstract_Extractor.R") 
-source("Text_Parser.R")
-source("Rule_Miner.R")
+
+source("PubMed_Extractor.R") 
+source("SpacyR_TextParser.R")
+source("Apriori_ARM_tool.R")
 
 # Step 1: Corpus Retrieval and Extraction
-myQuery <- "((complement system pathway) OR (complement system proteins)) AND (pathology OR pathologies)"
+myQuery <- "((complement system pathway) OR (complement system proteins)) AND (pathology OR pathologies) AND 1980:2010[dp]"
 
-raw_pub_info <- Abstract_Extractor(myQuery,500)
+raw_pub_info <- Abstract_Extractor(myQuery,1500)
 
 #Step 2: Natural Language Processing
 parsed_abstracts <- Text_Parser(raw_pub_info, venv_path = "C:\\Users\\Chris\\.virtualenvs\\Python-FsgYzr6a", 
             lang_model = "en_core_web_sm", 0.2)
 
 #Step 3: Mining Association Rules (this includes statistical filtering)
-rules <- ARM(parsed_abstracts$lemma, min_supp = 0.01, min_conf = 0.8, min_p = 0.05)
+rules <- ARM(parsed_abstracts, min_supp = 0.01, min_conf = 0.8, min_p = 0.05)
 
-df_rules <- DATAFRAME(rules)
-filt_rules <- which(df_rules$lift <= 2)
-df_rules <- df_rules[-filt_rules,]
+
+
+filt_rules <- which(rules$lift <= 2)
+rules <- rules[-filt_rules,]
 write.csv(df_rules, file = "sig_rules.csv")
 
-#removing significant rules that do not meet the lift requirements
 
-wordsearch <- df_rules[which(df_rules$RHS == "{protein}"),]
-freq = df_rules %>% count(RHS) %>% arrange(desc(n))
-frequency_dataframe2 = data_post %>% count(RHS) %>% arrange(desc(n))
-short_dataframe2 = head(frequency_dataframe2, 20)
+plot(rules, method = 'graph', measure = 'lift', shading = 'coverage')
+
+wordsearch <- rules[which(rules$RHS == "{c4d+}"),]
+
+freq = rules %>% count(RHS) %>% arrange(desc(n))
+short_dataframe2 = head(freq, 20)
 
 ggplot(short_dataframe2, aes(x = RHS, y = n, fill = RHS)) + geom_col() 
 
